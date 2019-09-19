bdc5106ea7bd3a308b3efa72c1e78a17de945c0f
cjmulholl <cjmulholl@gmail.com>
2019-09-19 08:53:53 -0400

    Change order of plots to get v_gas and p before code throws error (@ dM calc)

--- ShockedISM_ParamFile.py
+++ ShockedISM_ParamFile.py
@@ -66,7 +66,7 @@ R0_all = R0_all*(3.086*(10.0**18)) # pc --> cm
 print(vin_all)
 
 # SET WHICH ROW OF INITAL PARAM TO USE
-param_row = 18
+param_row = 1
 L_AGN=L_AGN_all[param_row]; vin=vin_all[param_row]; alpha=alpha_all[param_row]
 nH_0= nH_0_all[param_row]; R0 = R0_all[param_row]; Tin=Tin_all[param_row]
 
@@ -140,7 +140,7 @@ for i in range(5):
     vg1_r=[]; p1_r=[]
     N1=[]; N1.append(0)
 
-    cnt1=0
+    cnt1=0; Rc_ind1=0
 
     #Calculate vgas and density in different regions of shock bubble
     #---Values at boundary calculated in last bin before moving to next region
@@ -244,7 +244,7 @@ for i in range(5):
     vg2_r=[]; p2_r=[]
     N2=[]; N2.append(0)
 
-    cnt2=0
+    cnt2=0; Rc_ind2=0
 
     #Calculate vgas and density in different regions of shock bubble
     #---Values at boundary calculated in last bin before moving to next region
@@ -305,199 +305,6 @@ for i in range(5):
     vg_t2[i+1] = vg2_r
     p_t2[i+1] = p2_r
 
-"""CALCULATE VALUES BETWEEN TIMESTEPS (t and t+10yrs)"""
-
-dr1_dm={}; dr2_dm={}
-r1_dm={}; r2_dm={}
-v1_dm={}; v2_dm={}
-p1_dm={}; p2_dm={}
-N1_dm={}; N2_dm={}
-dv_dt={}
-
-r1_dm_cut={};r2_dm_cut={}
-v1_dm_cut={};v2_dm_cut={}
-dv_dt_cut={}
-
-for i in range(5):
-    #Calculate constant mass element dM using dRc=Rc(t2)-Rc(t1)
-    #---Remember to convert R values from pc back to cm to get correct units
-    dM=((((Rc_t1[i]+Rc_t1[i])*(3.086*(10.0**18)))/2.0)**2.0)*((p1_Rc[i]+p2_Rc[i])/2.0)*((Rc_t2[i]-Rc_t1[i])*(3.086*(10.0**18)))
-    #print(dM)
-
-    dr1=[]; dr1_sh=[]
-    r1=[]; r1_sh=[]
-    v1=[]; v1_sh=[]
-    p1=[];p1_sh=[]
-    N1=[];N1_sh=[]
-
-    r1_cut=[];r1_sh_cut=[]
-    v1_cut=[];v1_sh_cut=[]
-
-    #Calculate first of all values at Rc then will work outwards in both directions
-    dr1.append(Rc_t2[i]-Rc_t1[i]); dr1_sh.append(Rc_t2[i]-Rc_t1[i])
-    r1.append(Rc_t1[i]); r1_sh.append(Rc_t1[i])
-    v1.append(vg1_Rc[i]); v1_sh.append(vg1_Rc[i])
-    p1.append(p1_Rc[i]); p1_sh.append(p1_Rc[i])
-
-    dr2=[];dr2_sh=[]
-    r2=[]; r2_sh=[]
-    v2=[]; v2_sh=[]
-    p2=[]; p2_sh=[]
-    N2=[]; N2_sh=[]
-
-    r2_cut=[];r2_sh_cut=[]
-    v2_cut=[]; v2_sh_cut=[]
-
-    dr2.append(Rc_t2[i]-Rc_t1[i]); dr2_sh.append(Rc_t2[i]-Rc_t1[i])
-    r2.append(Rc_t2[i]); r2_sh.append(Rc_t2[i])
-    v2.append(vg2_Rc[i]); v2_sh.append(vg2_Rc[i])
-    p2.append(p2_Rc[i]); p2_sh.append(p2_Rc[i])
-
-    m=0;n=0
-    #Using constant dm, define dr working outwards from Rc for FIRST TIMESTEP (t)
-    #---Find velocities and densities that correspond to dr bins for t
-    for j in range(0,len(r_t1[i+1])-1):
-        #Working from Rc inwards to R1 (unshocked wind)
-        if r_t1[i+1][len(r_t1[i+1])-j-1] <= r1[m] :
-            dr1.append((dM/((((r_t1[i+1][len(r_t1[i+1])-j-1]*(3.086*(10.0**18)))**2.0)*(p_t1[i+1][len(r_t1[i+1])-j-1]))))/(3.086*(10.0**18)))
-            r1.append(r_t1[i+1][len(r_t1[i+1])-j-1]-dr1[m+1])
-            v1.append(vg_t1[i+1][len(r_t1[i+1])-j-1])
-            p1.append(p_t1[i+1][len(r_t1[i+1])-j-1])
-
-            #Define cut selection excluding region r<R1
-            if r1[m]>R1_t1[i]:
-                r1_cut.append(r1[m+1])
-                v1_cut.append(v1[m+1])
-
-                #Calculate N for defined dr bin (for timestep 1)
-                #--- Units p: [cm^-3], v: [km/s], dr: [pc] --> [cm]
-                N1.append((((p1[m]+p1[m+1])/2.0)/np.abs(v1[m]-v1[m+1]))*(dr1[m+1]*(3.086*(10.0**18))))
-
-            m+=1
-
-        #Working from Rc outwards to R2 (shocked wind)
-        if r_t1[i+1][j] >= r1_sh[n]:
-            dr1_sh.append((dM/((((r_t1[i+1][j]*(3.086*(10.0**18)))**2.0)*(p_t1[i+1][j]))))/(3.086*(10.0**18)))
-            r1_sh.append(r_t1[i+1][j]+dr1_sh[n+1])
-            v1_sh.append(vg_t1[i+1][j])
-            p1_sh.append(p_t1[i+1][j])
-
-            #Define cut selection excluding region r>R2
-            if r1_sh[n+1]<R2_t1[i]:
-                r1_sh_cut.append(r1_sh[n+1])
-                v1_sh_cut.append(v1_sh[n+1])
-
-                #Calculate N for defined dr bin (for timestep 1)
-                N1_sh.append((((p1_sh[n]+p1_sh[n+1])/2.0)/np.abs(v1_sh[n]-v1_sh[n+1]))*(dr1_sh[n+1]*(3.086*(10.0**18))))
-
-            n+=1
-
-    m=0;n=0
-    #Using constant dm, define dr working outwards from Rc for SECOND TIMESTEP (t+10yrs)
-    #---Find velocities and densities that correspond to dr bins for t
-    for j in range(0,len(r_t2[i+1])-1):
-        #Working from Rc inwards to R1 (unshocked wind)
-        if r_t2[i+1][len(r_t2[i+1])-j-1] <= r2[m] :
-            dr2.append((dM/((((r_t2[i+1][len(r_t2[i+1])-j-1]*(3.086*(10.0**18)))**2.0)*(p_t2[i+1][len(r_t2[i+1])-j-1]))))/(3.086*(10.0**18)))
-            r2.append(r_t2[i+1][len(r_t2[i+1])-j-1]-dr2[m+1])
-            v2.append(vg_t2[i+1][len(r_t2[i+1])-j-1])
-            p2.append(p_t2[i+1][len(r_t2[i+1])-j-1])
-
-            #Define cut selection excluding region r<R1
-            if r2[m]>R1_t2[i]:
-                r2_cut.append(r2[m+1])
-                v2_cut.append(v2[m+1])
-
-                #Calculate N for defined dr bin (for timestep 2)
-                N2.append((((p2[m]+p2[m+1])/2.0)/np.abs(v2[m]-v2[m+1]))*(dr2[m+1]*(3.086*(10.0**18))))
-
-            m+=1
-
-        #Working from Rc outwards to R2 (shocked wind)
-        if r_t2[i+1][j] >= r2_sh[n]:
-            dr2_sh.append((dM/((((r_t2[i+1][j]*(3.086*(10.0**18)))**2.0)*(p_t2[i+1][j]))))/(3.086*(10.0**18)))
-            r2_sh.append(r_t2[i+1][j]+dr2_sh[n+1])
-            v2_sh.append(vg_t2[i+1][j])
-            p2_sh.append(p_t2[i+1][j])
-
-            #Define cut selection excluding region r>R2
-            if r2_sh[n+1]<R2_t2[i]:
-                r2_sh_cut.append(r2_sh[n+1])
-                v2_sh_cut.append(v2_sh[n+1])
-
-                #Calculate N for defined dr bin (for timestep 2)
-                N2_sh.append((((p2_sh[n]+p2_sh[n+1])/2.0)/np.abs(v2_sh[n]-v2_sh[n+1]))*(dr2_sh[n+1]*(3.086*(10.0**18))))
-
-            n+=1
-
-    #SECOND TIMESTEP (t+10yrs) has one more bin at end than FIRST TIMESTEP (t) (for the cut values)...just remove last one
-    r2_sh_cut.pop(); v2_sh_cut.pop(); N2_sh.pop()
-
-    #Calculate dv/dt between FIRST AND SECOND TIMESTEP (t and t+10) before and after shock (boundary Rc)
-    dvdt=[];dvdt_sh=[]
-    dvdt_cut=[];dvdt_sh_cut=[]
-    for j in range(0,len(v1)):
-        #Check bin doesn't span over R1 boundary making v_gas jump
-        if v1[j] !=20000 and v2[j] ==20000:
-            dvdt_sh.append(0.0)
-        else:
-            dv= np.abs(v2[j]-v1[j])
-            dvdt.append(dv/10.0)
-
-            #Define cut selection excluding region r<R1
-            if r1[j]>R1_t1[i]:
-                dvdt_cut.append(dv/10.0)
-
-
-    for j in range(0,len(v1_sh)):
-        #Check bin doesn't span over R2 boundary making v_gas jump
-        if v1_sh[j] ==0 and v2_sh[j] !=0:
-            dvdt_sh.append(0.0)
-        else:
-            dv_sh = np.abs(v2_sh[j]-v1_sh[j])
-            dvdt_sh.append(dv_sh/10.0)
-
-            #Define cut selection excluding region r>R2
-            if r1_sh[j]<R2_t1[i]:
-                dvdt_sh_cut.append(dv_sh/10.0)
-
-    dvdt_sh_cut.pop()
-
-    #Add lists to dictionary for corresponding time
-    #---Reverse unshocked wind lists because worked inwards from Rc
-    dr1.reverse(); dr1.extend(dr1_sh); dr1_dm[i+1]=dr1
-    r1.reverse(); r1.extend(r1_sh); r1_dm[i+1]=r1
-    v1.reverse(); v1.extend(v1_sh); v1_dm[i+1]=v1
-    p1.reverse(); p1.extend(p1_sh); p1_dm[i+1]=p1
-    N1.reverse(); N1.extend(N1_sh); N1_dm[i+1]=N1
-
-    r1_cut.reverse(); r1_cut.extend(r1_sh_cut); r1_dm_cut[i+1]=r1_cut
-    v1_cut.reverse(); v1_cut.extend(v1_sh_cut); v1_dm_cut[i+1]=v1_cut
-
-    dr2.reverse(); dr2.extend(dr2_sh); dr2_dm[i+1]=dr2
-    r2.reverse(); r2.extend(r2_sh); r2_dm[i+1]=r2
-    v2.reverse(); v2.extend(v2_sh); v2_dm[i+1]=v2
-    p2.reverse(); p2.extend(p2_sh); p2_dm[i+1]=p2
-    N2.reverse(); N2.extend(N2_sh); N2_dm[i+1]=N2
-
-    r2_cut.reverse(); r2_cut.extend(r2_sh_cut); r2_dm_cut[i+1]=r2_cut
-    v2_cut.reverse(); v2_cut.extend(v2_sh_cut); v2_dm_cut[i+1]=v2_cut
-
-    dvdt.reverse(); dvdt.extend(dvdt_sh); dv_dt[i+1]=dvdt
-    dvdt_cut.reverse(); dvdt_cut.extend(dvdt_sh_cut); dv_dt_cut[i+1]=dvdt_cut
-
-#Print sizes to double check
-"""
-print(np.size(N1_dm[1])); print(np.size(r1_dm_cut[1])); print(np.size(v1_dm_cut[1]))
-print(np.size(dv_dt_cut[1]))
-print(np.size(N2_dm[1])); print(np.size(r2_dm_cut[1])); print(np.size(v2_dm_cut[1]))
-
-print(r1_dm[1][1])
-print(dr1_dm[1][1])
-print(p1_dm[1][1])
-print(v1_dm[1][2]-v1_dm[1][1])
-print(N1_dm[1][1])
-"""
 
 #---Plot R1, R2, snd Rc vs time
 plt.plot(np.log10(t1),np.log10(R1_t1),
@@ -1164,6 +971,203 @@ plt.title(r'$L_{AGN}(erg/s)=$'+str(L_AGN)+
           fontsize=10)
 plt.savefig('test_'+str(param_row)+'/ShockBubble_lognVSvgas.png',bbox_inches='tight')
 plt.show()
+
+"""CALCULATE VALUES BETWEEN TIMESTEPS (t and t+10yrs)"""
+
+dr1_dm={}; dr2_dm={}
+r1_dm={}; r2_dm={}
+v1_dm={}; v2_dm={}
+p1_dm={}; p2_dm={}
+N1_dm={}; N2_dm={}
+dv_dt={}
+
+r1_dm_cut={};r2_dm_cut={}
+v1_dm_cut={};v2_dm_cut={}
+dv_dt_cut={}
+
+for i in range(5):
+    #Calculate constant mass element dM using dRc=Rc(t2)-Rc(t1)
+    #---Remember to convert R values from pc back to cm to get correct units
+    dM=((((Rc_t2[i]+Rc_t1[i])*(3.086*(10.0**18)))/2.0)**2.0)*((p1_Rc[i]+p2_Rc[i])/2.0)*((Rc_t2[i]-Rc_t1[i])*(3.086*(10.0**18)))
+    #print(dM)
+
+    dr1=[]; dr1_sh=[]
+    r1=[]; r1_sh=[]
+    v1=[]; v1_sh=[]
+    p1=[];p1_sh=[]
+    N1=[];N1_sh=[]
+
+    r1_cut=[];r1_sh_cut=[]
+    v1_cut=[];v1_sh_cut=[]
+
+    #Calculate first of all values at Rc then will work outwards in both directions
+    dr1.append(Rc_t2[i]-Rc_t1[i]); dr1_sh.append(Rc_t2[i]-Rc_t1[i])
+    r1.append(Rc_t1[i]); r1_sh.append(Rc_t1[i])
+    v1.append(vg1_Rc[i]); v1_sh.append(vg1_Rc[i])
+    p1.append(p1_Rc[i]); p1_sh.append(p1_Rc[i])
+
+    dr2=[];dr2_sh=[]
+    r2=[]; r2_sh=[]
+    v2=[]; v2_sh=[]
+    p2=[]; p2_sh=[]
+    N2=[]; N2_sh=[]
+
+    r2_cut=[];r2_sh_cut=[]
+    v2_cut=[]; v2_sh_cut=[]
+
+    dr2.append(Rc_t2[i]-Rc_t1[i]); dr2_sh.append(Rc_t2[i]-Rc_t1[i])
+    r2.append(Rc_t2[i]); r2_sh.append(Rc_t2[i])
+    v2.append(vg2_Rc[i]); v2_sh.append(vg2_Rc[i])
+    p2.append(p2_Rc[i]); p2_sh.append(p2_Rc[i])
+
+    m=0;n=0
+    #Using constant dm, define dr working outwards from Rc for FIRST TIMESTEP (t)
+    #---Find velocities and densities that correspond to dr bins for t
+    for j in range(0,len(r_t1[i+1])-1):
+        #Working from Rc inwards to R1 (unshocked wind)
+        if r_t1[i+1][len(r_t1[i+1])-j-1] <= r1[m] :
+            dr1.append((dM/((((r_t1[i+1][len(r_t1[i+1])-j-1]*(3.086*(10.0**18)))**2.0)*(p_t1[i+1][len(r_t1[i+1])-j-1]))))/(3.086*(10.0**18)))
+            r1.append(r_t1[i+1][len(r_t1[i+1])-j-1]-dr1[m+1])
+            v1.append(vg_t1[i+1][len(r_t1[i+1])-j-1])
+            p1.append(p_t1[i+1][len(r_t1[i+1])-j-1])
+
+            #Define cut selection excluding region r<R1
+            if r1[m]>R1_t1[i]:
+                r1_cut.append(r1[m+1])
+                v1_cut.append(v1[m+1])
+
+                #Calculate N for defined dr bin (for timestep 1)
+                #--- Units p: [cm^-3], v: [km/s], dr: [pc] --> [cm]
+                N1.append((((p1[m]+p1[m+1])/2.0)/np.abs(v1[m]-v1[m+1]))*(dr1[m+1]*(3.086*(10.0**18))))
+
+            m+=1
+
+        #Working from Rc outwards to R2 (shocked wind)
+        if r_t1[i+1][j] >= r1_sh[n]:
+            dr1_sh.append((dM/((((r_t1[i+1][j]*(3.086*(10.0**18)))**2.0)*(p_t1[i+1][j]))))/(3.086*(10.0**18)))
+            r1_sh.append(r_t1[i+1][j]+dr1_sh[n+1])
+            v1_sh.append(vg_t1[i+1][j])
+            p1_sh.append(p_t1[i+1][j])
+
+            #Define cut selection excluding region r>R2
+            if r1_sh[n+1]<R2_t1[i]:
+                r1_sh_cut.append(r1_sh[n+1])
+                v1_sh_cut.append(v1_sh[n+1])
+
+                #Calculate N for defined dr bin (for timestep 1)
+                N1_sh.append((((p1_sh[n]+p1_sh[n+1])/2.0)/np.abs(v1_sh[n]-v1_sh[n+1]))*(dr1_sh[n+1]*(3.086*(10.0**18))))
+
+            n+=1
+
+    m=0;n=0
+    #Using constant dm, define dr working outwards from Rc for SECOND TIMESTEP (t+10yrs)
+    #---Find velocities and densities that correspond to dr bins for t
+    for j in range(0,len(r_t2[i+1])-1):
+        #Working from Rc inwards to R1 (unshocked wind)
+        if r_t2[i+1][len(r_t2[i+1])-j-1] <= r2[m] :
+            dr2.append((dM/((((r_t2[i+1][len(r_t2[i+1])-j-1]*(3.086*(10.0**18)))**2.0)*(p_t2[i+1][len(r_t2[i+1])-j-1]))))/(3.086*(10.0**18)))
+            r2.append(r_t2[i+1][len(r_t2[i+1])-j-1]-dr2[m+1])
+            v2.append(vg_t2[i+1][len(r_t2[i+1])-j-1])
+            p2.append(p_t2[i+1][len(r_t2[i+1])-j-1])
+
+            #Define cut selection excluding region r<R1
+            if r2[m]>R1_t2[i]:
+                r2_cut.append(r2[m+1])
+                v2_cut.append(v2[m+1])
+
+                #Calculate N for defined dr bin (for timestep 2)
+                N2.append((((p2[m]+p2[m+1])/2.0)/np.abs(v2[m]-v2[m+1]))*(dr2[m+1]*(3.086*(10.0**18))))
+
+            m+=1
+
+        #Working from Rc outwards to R2 (shocked wind)
+        if r_t2[i+1][j] >= r2_sh[n]:
+            dr2_sh.append((dM/((((r_t2[i+1][j]*(3.086*(10.0**18)))**2.0)*(p_t2[i+1][j]))))/(3.086*(10.0**18)))
+            r2_sh.append(r_t2[i+1][j]+dr2_sh[n+1])
+            v2_sh.append(vg_t2[i+1][j])
+            p2_sh.append(p_t2[i+1][j])
+
+            #Define cut selection excluding region r>R2
+            if r2_sh[n+1]<R2_t2[i]:
+                r2_sh_cut.append(r2_sh[n+1])
+                v2_sh_cut.append(v2_sh[n+1])
+
+                #Calculate N for defined dr bin (for timestep 2)
+                N2_sh.append((((p2_sh[n]+p2_sh[n+1])/2.0)/np.abs(v2_sh[n]-v2_sh[n+1]))*(dr2_sh[n+1]*(3.086*(10.0**18))))
+
+            n+=1
+
+    #SECOND TIMESTEP (t+10yrs) has one more bin at end than FIRST TIMESTEP (t) (for the cut values)...just remove last one
+    r2_sh_cut.pop(); v2_sh_cut.pop(); N2_sh.pop()
+
+    #Calculate dv/dt between FIRST AND SECOND TIMESTEP (t and t+10) before and after shock (boundary Rc)
+    dvdt=[];dvdt_sh=[]
+    dvdt_cut=[];dvdt_sh_cut=[]
+    for j in range(0,len(v1)):
+        #Check bin doesn't span over R1 boundary making v_gas jump
+        if v1[j] !=20000 and v2[j] ==20000:
+            dvdt_sh.append(0.0)
+        else:
+            dv= np.abs(v2[j]-v1[j])
+            dvdt.append(dv/10.0)
+
+            #Define cut selection excluding region r<R1
+            if r1[j]>R1_t1[i]:
+                dvdt_cut.append(dv/10.0)
+
+
+    for j in range(0,len(v1_sh)):
+        #Check bin doesn't span over R2 boundary making v_gas jump
+        if v1_sh[j] ==0 and v2_sh[j] !=0:
+            dvdt_sh.append(0.0)
+        else:
+            dv_sh = np.abs(v2_sh[j]-v1_sh[j])
+            dvdt_sh.append(dv_sh/10.0)
+
+            #Define cut selection excluding region r>R2
+            if r1_sh[j]<R2_t1[i]:
+                dvdt_sh_cut.append(dv_sh/10.0)
+
+    dvdt_sh_cut.pop()
+
+    #Add lists to dictionary for corresponding time
+    #---Reverse unshocked wind lists because worked inwards from Rc
+    dr1.reverse(); dr1.extend(dr1_sh); dr1_dm[i+1]=dr1
+    r1.reverse(); r1.extend(r1_sh); r1_dm[i+1]=r1
+    v1.reverse(); v1.extend(v1_sh); v1_dm[i+1]=v1
+    p1.reverse(); p1.extend(p1_sh); p1_dm[i+1]=p1
+    N1.reverse(); N1.extend(N1_sh); N1_dm[i+1]=N1
+
+    r1_cut.reverse(); r1_cut.extend(r1_sh_cut); r1_dm_cut[i+1]=r1_cut
+    v1_cut.reverse(); v1_cut.extend(v1_sh_cut); v1_dm_cut[i+1]=v1_cut
+
+    dr2.reverse(); dr2.extend(dr2_sh); dr2_dm[i+1]=dr2
+    r2.reverse(); r2.extend(r2_sh); r2_dm[i+1]=r2
+    v2.reverse(); v2.extend(v2_sh); v2_dm[i+1]=v2
+    p2.reverse(); p2.extend(p2_sh); p2_dm[i+1]=p2
+    N2.reverse(); N2.extend(N2_sh); N2_dm[i+1]=N2
+
+    r2_cut.reverse(); r2_cut.extend(r2_sh_cut); r2_dm_cut[i+1]=r2_cut
+    v2_cut.reverse(); v2_cut.extend(v2_sh_cut); v2_dm_cut[i+1]=v2_cut
+
+    dvdt.reverse(); dvdt.extend(dvdt_sh); dv_dt[i+1]=dvdt
+    dvdt_cut.reverse(); dvdt_cut.extend(dvdt_sh_cut); dv_dt_cut[i+1]=dvdt_cut
+
+#Print sizes to double check
+"""
+print(np.size(N1_dm[1])); print(np.size(r1_dm_cut[1])); print(np.size(v1_dm_cut[1]))
+print(np.size(dv_dt_cut[1]))
+print(np.size(N2_dm[1])); print(np.size(r2_dm_cut[1])); print(np.size(v2_dm_cut[1]))
+
+print(r1_dm[1][1])
+print(dr1_dm[1][1])
+print(p1_dm[1][1])
+print(v1_dm[1][2]-v1_dm[1][1])
+print(N1_dm[1][1])
+"""
+
+
+"""MAIN N & dv/dt plots """
 
 #Plot log(dv/dt) vs log(R) (at all t) before and after shock
 #plt.plot([], [], ' ', label=r'$v_w=20000km/s$')
